45f5f5c2177a6e4dab2a24399a112987aa9aa39b
amiria703 <hi@amiria703.ir>
2023-03-18 01:38:20 +0330

    Small fix in contact pages.

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,7 +3,7 @@
     <Title>Contact</Title>
     <div><NuxtLink to="mailto:hi@amiria703.ir">Email: hi@amiria703.ir</NuxtLink></div>
     <div><NuxtLink to="tel:+989229716813">Phone: +989229716813</NuxtLink></div>
-    <div><NuxtLink to="https://github.com/amiria703">GitHub: GitHub.com/amiria703</NuxtLink></div>
+    <div><NuxtLink to="https://github.com/amiria703">GitHub: @amiria703</NuxtLink></div>
     <div><NuxtLink to="https://t.me/amiria703">Telegram: @amiria703</NuxtLink></div>
   </main>
 </template>
